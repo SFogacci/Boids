c2376015b053782e8928a1094c42a94d088ffc90
SFogacci <samuelefogacci@gmail.com>
2024-05-06 12:41:17 +0200

    Initial commit

--- boids.cpp
+++ boids.cpp
@@ -1,6 +1,18 @@
 #include "boids.hpp"
+#include <cmath>
 #include <vector>
 
 
+int main()
+{
+  bd::Boid b1{{0, 0}, {1, 1}};
+  bd::Boid b2{{1, 0}, {-1, 1}};
+  bd::Boid b3{{0, 1}, {1, -1}};
+  bd::Boid b4{{1, -1}, {-1, -1}};
 
-int main();
+  std::vector<bd::Boid> flock{};
+  flock.push_back(b1);
+  flock.push_back(b2);
+  flock.push_back(b3);
+  flock.push_back(b4);
+};
