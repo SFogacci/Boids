500b0be08009797d3e1050d6132fcc6063cf906c
Cdipiazza <camilla.dipiazza@gmail.com>
2024-06-24 20:49:40 +0200

    Small change in createBirds function in order not to to create a whole boid and then discard it

--- input.cpp
+++ input.cpp
@@ -9,10 +9,11 @@ auto createBirds(const std::size_t& n)
   birds.reserve(n);
 
   for (std::size_t i = 0; i != n; ++i) {
+    Boid bird{generateCoordinates(0.f, 900.f), {}};
     while (true) {
-      Boid bird{generateCoordinates(0.f, 900.f),
-                generateCoordinates(-5.f, 5.f)};
-      if (norm(bird.getVelocity()) > 0.5) {
+      auto velocity{generateCoordinates(-5.f, 5.f)};
+      if (norm(velocity) > 0.5) {
+        bird.setVelocity(velocity);
         birds.push_back(bird);
         break;
       }
