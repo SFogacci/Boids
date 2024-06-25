cfcf16100b8307662962b276e0a94b2e833fabb8
Lorenzo-Antolini <antolini650@gmail.com>
2024-06-25 15:04:24 +0200

    Introdotto costante globale dt, dato raggio di visione maggiore ai predatori, definzione di overlapping

--- boid.cpp
+++ boid.cpp
@@ -6,6 +6,8 @@
 
 namespace bd {
 
+const float dt{1.f}; // il vecchio consistency factor
+
 bool operator==(Boid const& a, Boid const& b)
 {
   return (a.getVelocity() == b.getVelocity()
@@ -14,16 +16,16 @@ bool operator==(Boid const& a, Boid const& b)
 
 void Boid::correct_borders()
 {
-  if (position_.x > 900.f) {
-    position_.x -= 900.f;
+  if (position_.x > static_cast<float>(w_window)) {
+    position_.x -= static_cast<float>(w_window);
   } else if (position_.x < 0.f) {
-    position_.x += 900.f;
+    position_.x += static_cast<float>(w_window);
   }
-  if (position_.y > 900.f) {
-    position_.y -= 900.f;
+  if (position_.y > static_cast<float>(h_window)) {
+    position_.y -= static_cast<float>(h_window);
   }
   if (position_.y < 0.f) {
-    position_.y += 900.f;
+    position_.y += static_cast<float>(h_window);
   } // c'è un problema sulla y per colpa della barra in alto che copre
 }
 
@@ -41,7 +43,7 @@ bool Boid::hasNeighbour(Boid const& b, float d) const
 
 void Boid::biological_limits()
 {
-  float maximum_speed = 5.f; // 500.f
+  const float maximum_speed = 5.f; // 500.f
   if (norm(velocity_) > maximum_speed) {
     normalize(velocity_, maximum_speed);
   }
@@ -55,106 +57,106 @@ void Boid::biological_limits()
 //   }
 // }
 
-void Flock::predator_evolution(Predator& p)
+Predator Flock::predator_evolution(Predator const& p) const
 {
+  Predator copy = p;
   Vector center_of_mass{};
-  const float d = flock_parameters_.d;
+  const float d = 2.f * flock_parameters_.d; // gli do 2 volte la visione dei boid, così vede più lontano (se è un predatore si sarà evoluto in modo da vedere meglio le prede, no?)
   std::vector<Boid> preys;
-  std::for_each(flock_.begin(), flock_.end(),
-                [p, d, &preys, &center_of_mass](Boid const& b) {
-                  if (p.hasNeighbour(b, d)) {
-                    center_of_mass += b.getPosition();
-                    preys.push_back(b);
-                  };
-                });
 
-  Parameters par{0.f, 0.f,         flock_parameters_.d, flock_parameters_.ds,
-                 1.f, preys.size()};
-  // Flock poor_preys(preys, par);
+  std::for_each(flock_.begin(), flock_.end(), [p, d, &preys, &center_of_mass](Boid const& b) {
+    if (p.hasNeighbour(b, d)) { // si potrebbe usare anche copy anzichè p, ma dato che non è modifictao non da problemi
+      center_of_mass += b.getPosition();
+      preys.push_back(b);
+    };
+  });
 
-  if (preys.size() >= 1) {
-    float consistency_factor{1.f}; // 60.f
-    Vector hunting =
-        (center_of_mass / static_cast<float>(preys.size()) - p.getPosition())
-        / 100.f; // No factor
-    p.setPosition(p.getPosition() + hunting / consistency_factor);
-    p.setVelocity(p.getVelocity() + hunting);
+  // Parameters par{0.f, 0.f, flock_parameters_.d, flock_parameters_.ds, 1.f, preys.size()};
+  if (preys.size() > 0) {
+    Vector hunting = (center_of_mass / static_cast<float>(preys.size()) - p.getPosition()) / 100.f;
+    copy.setVelocity(p.getVelocity() + hunting);
+    copy.biological_limits();
+
+    copy.setPosition(p.getPosition() + p.getVelocity() / dt); // così è anche già aggiunto hunting e inoltre aggiungiamo anche la velcoità del predatore, cosa che prima non avveniva
+
   } else {
-    float consistency_factor{1.f}; // 60.f
-    p.setPosition(p.getPosition() + p.getVelocity() / consistency_factor);
+    copy.setPosition(p.getPosition() + p.getVelocity() / dt);
   }
-  p.correct_borders();
-  p.biological_limits();
+  copy.correct_borders();
+
+  return copy;
 }
 
-void Flock::evolution()
+void Flock::evolution(Predator const& p)
 {
-  std::vector<Boid> modified_flock;
+std::vector<Boid> modified_flock;
   modified_flock.reserve(flock_.size());
 
   for (Boid const& boid : flock_) {
-    const auto neighbours = static_cast<float>(
-        std::count_if(flock_.begin(), flock_.end(), [&](auto const& other) {
-          return boid.hasNeighbour(other, flock_parameters_.d);
-        }));
-
+    const auto neighbours = static_cast<float>(std::count_if(flock_.begin(), flock_.end(), [&](auto const& other) { return boid.hasNeighbour(other, flock_parameters_.d); }));
+    Corrections corrections;
     if (neighbours != 0.f) {
-      auto corrections = std::accumulate(
-          flock_.begin(), flock_.end(), Corrections{},
-          [&](auto& sums, auto const& other) {
-            if (boid.hasNeighbour(other, flock_parameters_.d)) {
-              sums.alignment += other.getVelocity();
-              sums.cohesion += other.getPosition();
-              if (boid.hasNeighbour(other, flock_parameters_.ds)) {
-                auto distance = other.getPosition() - boid.getPosition();
-                normalize(distance, flock_parameters_.ds / norm(distance));
-                sums.separation += distance; // versore e normalizzazione su ds
-              }
-            }
-            return sums;
-          });
+      corrections = std::accumulate(flock_.begin(), flock_.end(), Corrections{}, [&](auto& sums, auto const& other) {
+        if (boid.hasNeighbour(other, flock_parameters_.d)) {
+          sums.alignment += other.getVelocity();
+          sums.cohesion += other.getPosition();
+          if (boid.hasNeighbour(other, flock_parameters_.ds)) {
+            auto distance = other.getPosition() - boid.getPosition();
+            normalize(distance, flock_parameters_.ds / norm(distance));
+            std::cout << " distance = " << std::hypot(distance.x, distance.y) << '\n';
+            sums.separation += distance; // versore e normalizzazione su ds
+          }
+        }
+        return sums;
+      });
 
-      corrections.alignment =
-          corrections.alignment / neighbours - boid.getVelocity();
-      corrections.cohesion =
-          corrections.cohesion / neighbours - boid.getPosition();
+      corrections.alignment = corrections.alignment / neighbours - boid.getVelocity();
+      corrections.cohesion  = corrections.cohesion / neighbours - boid.getPosition();
 
-      auto newVel =
-          boid.getVelocity() + flock_parameters_.a * corrections.alignment
-          + flock_parameters_.c * corrections.cohesion // /100.F (No factor)
-          - flock_parameters_.s * corrections.separation;
+      if (p.hasNeighbour(boid, flock_parameters_.d)) {
+        corrections.separation_predator += (flock_parameters_.d / norm(p.getPosition() - boid.getPosition()) * flock_parameters_.s * p.getVelocity());
+      } // l'obiettivo è farlo funzionare come separation, al momento lo lascio così
 
-      float consistency_factor{1.f}; // 60.f
-      Boid modified_boid(boid.getPosition() + newVel / (consistency_factor),
-                         newVel);
+      // Vector corrections_predator;
+      // std::vector<Boid> preys;
+      // const float d = flock_parameters_.d;
 
-      // if (std::find_if(preys.begin(), preys.end(),
-      //                  [boid](Boid const& prey) { return prey == boid; })
-      //     != preys.end()) {
-      //   newVel = (flock_parameters_.d / norm(p.getPosition() -
-      //   boid.getPosition())
-      //             * par.s * p.getVelocity());
+      // std::for_each(flock_.begin(), flock_.end(), [p, d, &preys](Boid const& b) {
+      //   if (p.hasNeighbour(b, d)) {
+      //     preys.push_back(b);
+      //   };
+      // });
+
+      // if (std::find_if(preys.begin(), preys.end(), [boid](Boid const& prey) { return prey == boid; }) != preys.end()) {
+      //   corrections_predator = (flock_parameters_.d / norm(p.getPosition() - boid.getPosition()) * flock_parameters_.s * p.getVelocity()); // il flock_parameters_.s è il vecchio par.s
       // }
 
-      std::for_each(
-          flock_.begin(), flock_.end(), [&modified_boid](Boid const& other) {
-            if (other == modified_boid) {
-              modified_boid.setPosition(modified_boid.getPosition()
-                                        + generateCoordinates(-1.f, 1.f));
-            };
-          });
+      auto newVel = boid.getVelocity() + flock_parameters_.a * corrections.alignment + flock_parameters_.c * corrections.cohesion // /100.F (No factor)
+                  - flock_parameters_.s * corrections.separation + corrections.separation_predator;
+
+      Boid modified_boid(boid.getPosition() + newVel / (dt), newVel);
+
+      overlapping(modified_boid); // io la ho messa membro di flock, si potrebbe anche mettere membro di Boid
+                                  // però anziche passare un boid dovrei passare il flock
+
       modified_boid.biological_limits();
       modified_boid.correct_borders();
       modified_flock.push_back(modified_boid);
     } else {
-      float consistency_factor{1.f}; // 60.f
-      Boid modified_boid(boid.getPosition()
-                             + boid.getVelocity() / (consistency_factor),
-                         boid.getVelocity());
+      Boid modified_boid(boid.getPosition() + boid.getVelocity() / (dt), boid.getVelocity());
       modified_flock.push_back(modified_boid);
     }
   }
 
   flock_ = modified_flock;
+}
+
+void Flock::overlapping(Boid& modified_boid)
+{
+  std::for_each(flock_.begin(), flock_.end(), [&modified_boid](Boid const& other) {
+    if (other == modified_boid) {
+      modified_boid.setPosition(modified_boid.getPosition() + generateCoordinates(-1.f, 1.f));
+    };
+  });
 }
 } // namespace bd
