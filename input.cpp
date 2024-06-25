b74c8efef4d15feba94873c046536df5f09ccf2e
Lorenzo-Antolini <antolini650@gmail.com>
2024-06-25 15:00:55 +0200

    Piccoli cambiamenti. Nota: ancora da sistemare l'input che deve variare in base ad altezza e larghezza schermo

--- input.cpp
+++ input.cpp
@@ -9,7 +9,7 @@ auto createBirds(const std::size_t& n)
   birds.reserve(n);
 
   for (std::size_t i = 0; i != n; ++i) {
-    Boid bird{generateCoordinates(0.f, 900.f), {}};
+    Boid bird{generateCoordinates(0.f, static_cast<float>(h_window)), {}}; // sarebbe da modificare in modo che la posizione in x sia al masismo w_window e in y h_window
     while (true) {
       auto velocity{generateCoordinates(-5.f, 5.f)};
       if (norm(velocity) > 0.5) {
@@ -24,7 +24,7 @@ auto createBirds(const std::size_t& n)
 
 auto createPredators()
 {
-  Predator bird{generateCoordinates(0.f, 900.f),
+  Predator bird{generateCoordinates(0.f, static_cast<float>(h_window)),
                 generateCoordinates(-5.f, 5.f)};
   return bird;
 }
