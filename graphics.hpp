69e94d3ec8696ec140aee03eef8205040e0ef7ca
Lorenzo-Antolini <antolini650@gmail.com>
2024-06-25 15:00:10 +0200

    Piccoli cambiamenti

--- graphics.hpp
+++ graphics.hpp
@@ -2,12 +2,11 @@
 #define GRAPHICS_HPP
 
 #include "boid.hpp"
-#include "SFML/Graphics.hpp"
 
 namespace bd {
 sf::ConvexShape setShape(Boid const&);
 sf::ConvexShape setShape(Predator const&);
-void gameLoop(bd::Flock& flock, Predator &p);
+void gameLoop(bd::Flock&, Predator &);
 } // namespace bd
 
 #endif 
