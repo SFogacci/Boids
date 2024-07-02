531cfe315eac90e7ad33358de7bcc4150c597b8a
Lorenzo-Antolini <antolini650@gmail.com>
2024-07-02 18:22:57 +0200

    Cambiata leggermente la forma dei boid e predatore, cambiato il colore dello sfondo

--- graphics.cpp
+++ graphics.cpp
@@ -9,38 +9,38 @@ sf::ConvexShape birdShape;
 sf::ConvexShape predatorShape;
 sf::ConvexShape setShape(bd::Boid const& b) // passaggio by const ref
 {
-  if (b.isPredator()) {
-    predatorShape.setPointCount(4);
-    predatorShape.setPoint(
-        0, sf::Vector2f(b.getPosition().x - 15.f, b.getPosition().y - 10.f));
-    predatorShape.setPoint(
-        1, sf::Vector2f(b.getPosition().x + 15.f, b.getPosition().y));
-    predatorShape.setPoint(
-        2, sf::Vector2f(b.getPosition().x - 15.f, b.getPosition().y + 10.f));
-    predatorShape.setPoint(
-        3, sf::Vector2f(b.getPosition().x - 5.f, b.getPosition().y));
-    predatorShape.setFillColor(sf::Color::Green);
-    predatorShape.setOutlineColor(sf::Color::White);
-    predatorShape.setOutlineThickness(1.f);
-    predatorShape.setPosition(b.getPosition().x, b.getPosition().y);
-    predatorShape.setOrigin(b.getPosition().x, b.getPosition().y);
-    predatorShape.setRotation(b.getOrientation());
-    return predatorShape;
-  } else {
+  if (!b.isPredator()) {
     birdShape.setPointCount(3);
     birdShape.setPoint(
-        0, sf::Vector2f(b.getPosition().x - 10.f, b.getPosition().y - 5.f));
+        0, sf::Vector2f(b.getPosition().x - 5.f, b.getPosition().y - 2.5f));
     birdShape.setPoint(
-        1, sf::Vector2f(b.getPosition().x + 10.f, b.getPosition().y));
+        1, sf::Vector2f(b.getPosition().x + 5.f, b.getPosition().y));
     birdShape.setPoint(
-        2, sf::Vector2f(b.getPosition().x - 10.f, b.getPosition().y + 5.f));
-    birdShape.setFillColor(sf::Color::Red);
-    birdShape.setOutlineColor(sf::Color::White);
+        2, sf::Vector2f(b.getPosition().x - 5.f, b.getPosition().y + 2.5f));
+    birdShape.setFillColor(sf::Color::Green);
+    birdShape.setOutlineColor(sf::Color::Black);
     birdShape.setOutlineThickness(1.f);
     birdShape.setPosition(b.getPosition().x, b.getPosition().y);
     birdShape.setOrigin(b.getPosition().x, b.getPosition().y);
     birdShape.setRotation(b.getOrientation());
     return birdShape;
+  } else {
+    predatorShape.setPointCount(4);
+    predatorShape.setPoint(
+        0, sf::Vector2f(b.getPosition().x - 7.5f, b.getPosition().y - 5.f));
+    predatorShape.setPoint(
+        1, sf::Vector2f(b.getPosition().x + 7.5f, b.getPosition().y));
+    predatorShape.setPoint(
+        2, sf::Vector2f(b.getPosition().x - 7.5f, b.getPosition().y + 5.f));
+    predatorShape.setPoint(
+        3, sf::Vector2f(b.getPosition().x - 2.5f, b.getPosition().y));
+    predatorShape.setFillColor(sf::Color::Red);
+    predatorShape.setOutlineColor(sf::Color::Black);
+    predatorShape.setOutlineThickness(1.f);
+    predatorShape.setPosition(b.getPosition().x, b.getPosition().y);
+    predatorShape.setOrigin(b.getPosition().x, b.getPosition().y);
+    predatorShape.setRotation(b.getOrientation());
+    return predatorShape;
   }
 }
 
@@ -49,8 +49,10 @@ void gameLoop(Flock& flock, Boid& p)
   sf::ContextSettings settings;
   settings.antialiasingLevel = 8;
 
-  sf::RenderWindow window(sf::VideoMode(w_window, h_window), "Boids",
-                          sf::Style::Default, settings);
+  sf::RenderWindow window(
+      sf::VideoMode(static_cast<unsigned int>(windowDimensions.x),
+                    static_cast<unsigned int>(windowDimensions.y)),
+      "Boids", sf::Style::Default, settings);
   window.setFramerateLimit(60);
   window.setPosition(sf::Vector2i(0, 0));
 
@@ -72,7 +74,8 @@ void gameLoop(Flock& flock, Boid& p)
     flock.evolution(p);
     p = modified_predator;
 
-    window.clear();
+    window.clear(
+        sf::Color(113, 188, 225, 255)); // è il colore del cielo (sereno)
 
     window.draw(setShape(p));
     std::for_each(flock.getFlock().begin(), flock.getFlock().end(),
