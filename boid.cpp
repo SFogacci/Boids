e6b765719b0bdb3ecf8fb854e0fb31e3546272f2
Lucabattazzi <163546006+Lucabattazzi@users.noreply.github.com>
2024-07-04 10:58:36 +0200

    Separation dal predatore normalizzata

--- boid.cpp
+++ boid.cpp
@@ -78,8 +78,11 @@ void Flock::evolution(Boid const& p)
   for (Boid const& boid : flock_) {
     Boid modified_boid(boid);
     if (boid.hasNeighbour(p, flock_parameters_.d)) {
-      const auto separation_predator = flock_parameters_.s * toroidalDifference(boid.getPosition(), p.getPosition());
-      modified_boid.setVelocity(boid.getVelocity() + separation_predator);
+      const auto predator_distance = toroidalDifference(boid.getPosition(), p.getPosition());
+      normalize(predator_distance, flock_parameters_.d / norm(predator_distance));
+      modified_boid.setVelocity(boid.getVelocity() + flock_parameters_.s *predator_distance);
+      // const auto separation_predator = flock_parameters_.s * toroidalDifference(boid.getPosition(), p.getPosition());
+      // modified_boid.setVelocity(boid.getVelocity() + separation_predator);
     }
 
     const auto neighbours = static_cast<float>(std::count_if(flock_.begin(), flock_.end(), [&](auto const& other) { return boid.hasNeighbour(other, flock_parameters_.d); }));
